51e8dc4e839b396141f8cf3575f73fae53f16e33
william <william_zxs@163.com>
2022-10-24 20:45:12 +0800

    add err handle

--- cron.go
+++ cron.go
@@ -11,12 +11,16 @@ func RunCron(conf *Config) {
 	for _, event := range conf.Events {
 		fmt.Println("添加定时任务\n", event.Spec, event.Cron)
 		_, err = c.AddFunc(event.Cron, func() {
-			conf.Mail.SendMail(event.Spec, "")
+			err := conf.Mail.SendMail(event.Spec, "")
+			if err != nil {
+				fmt.Println("send mail err", err)
+			}
 		})
+		if err != nil {
+			panic(err)
+		}
 	}
-	if err != nil {
-		fmt.Println("error", err)
-	}
+	
 	// 启动一个新的 goroutine 做循环检测
 	c.Start()
 	select {}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import "flag"
+import (
+	"flag"
+)
 
 func main() {
 	confFile := flag.String("f", "config.yaml", "请指定配置文件")
 	flag.Parse()
 	conf := InitConfig(*confFile)
 	RunCron(conf)
+
 }
